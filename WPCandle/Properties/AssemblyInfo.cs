e000ac6c982cc3342f58f97a53f10b2e8263580f
Vergeol <vergeol@yahoo.fr>
2013-10-05 23:05:08 +0200

    Enough for today. Info pane somewhat ready

--- WPCandle/Properties/AssemblyInfo.cs
+++ WPCandle/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WPCandle")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © Guilbored  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
