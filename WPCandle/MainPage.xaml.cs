6ce810ac80e3eb7d63bb25660b8a73fb9f8d9bb2
Vergeol <vergeol@yahoo.fr>
2013-10-05 22:05:09 +0200

    info buttons and overlay

--- WPCandle/MainPage.xaml.cs
+++ WPCandle/MainPage.xaml.cs
@@ -16,13 +16,16 @@ namespace WPCandle
 {
     public partial class MainPage : PhoneApplicationPage
     {
-       
+
+        PageOrientation lastOrientation;
 
         // Constructor
         public MainPage()
         {
             InitializeComponent();
-            Loaded+=MainPage_Loaded;
+            Loaded += MainPage_Loaded;
+            OrientationChanged += new EventHandler<OrientationChangedEventArgs>(MainPage_OrientationChanged);
+            lastOrientation = this.Orientation;
         }
 
 
@@ -32,5 +35,54 @@ namespace WPCandle
         }
 
 
+
+        void MainPage_OrientationChanged(object sender, OrientationChangedEventArgs e)
+        {
+            PageOrientation newOrientation = e.Orientation;
+
+            // Orientations are (clockwise) 'PortraitUp', 'LandscapeRight', 'LandscapeLeft'
+            RotateTransition transitionElement = new RotateTransition();
+
+            switch (newOrientation)
+            {
+                case PageOrientation.Landscape:
+                case PageOrientation.LandscapeRight:
+                    // Come here from PortraitUp (i.e. clockwise) or LandscapeLeft?
+                    if (lastOrientation == PageOrientation.PortraitUp)
+                        transitionElement.Mode = RotateTransitionMode.In90Counterclockwise;
+                    else
+                        transitionElement.Mode = RotateTransitionMode.In180Clockwise;
+                    break;
+                case PageOrientation.LandscapeLeft:
+                    // Come here from LandscapeRight or PortraitUp?
+                    if (lastOrientation == PageOrientation.LandscapeRight)
+                        transitionElement.Mode = RotateTransitionMode.In180Counterclockwise;
+                    else
+                        transitionElement.Mode = RotateTransitionMode.In90Clockwise;
+                    break;
+                case PageOrientation.Portrait:
+                case PageOrientation.PortraitUp:
+                    // Come here from LandscapeLeft or LandscapeRight?
+                    if (lastOrientation == PageOrientation.LandscapeLeft)
+                        transitionElement.Mode = RotateTransitionMode.In90Counterclockwise;
+                    else
+                        transitionElement.Mode = RotateTransitionMode.In90Clockwise;
+                    break;
+                default:
+                    break;
+            }
+
+            // Execute the transition
+            PhoneApplicationPage phoneApplicationPage = (PhoneApplicationPage)(((PhoneApplicationFrame)Application.Current.RootVisual)).Content;
+            ITransition transition = transitionElement.GetTransition(phoneApplicationPage);
+            transition.Completed += delegate
+            {
+                transition.Stop();
+            };
+            transition.Begin();
+
+            lastOrientation = newOrientation;
+
+        }
     }
 }
